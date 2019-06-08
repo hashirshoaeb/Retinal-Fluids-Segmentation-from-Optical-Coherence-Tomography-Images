9a729718918a24e0caa318d59d70ade8d332cc54
hashirshoaeb <hashirshoaeb@gmail.com>
2019-06-08 23:07:52 +0500

    .mat files

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 import  numpy as np
 import cv2 as cv
+import scipy.io
+
+mat = scipy.io.loadmat('path/Subject_02.mat')
+print (mat.keys())
+print(mat['__version__'])
+for i in mat.keys():
+    print(mat[i])
+
 
