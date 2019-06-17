4e3bb4be3191b2cf7a5c023b2984fc677f827adb
hashirshoaeb <hashirshoaeb@gmail.com>
2019-06-17 21:07:20 +0500

    got images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import  numpy as np
+import numpy as np
 import cv2 as cv
 import scipy.io
+import matplotlib.pyplot as plt
+import math as m
 
 mat = scipy.io.loadmat('path/Subject_01.mat',struct_as_record = False)
 # https://github.com/scipy/scipy/blob/v1.3.0/scipy/io/matlab/mio.py#L83-L225
@@ -12,5 +14,25 @@ for key, value in final_mat.items():
     print(key ,"corrosponrds to", np.shape(value))
     # cv.imwrite(key+".png",value)
 
+images = final_mat['images']
+# 'images' has 61 gray level images with dimension 496, 768
+row, col, total_images = np.shape(images)
+print(total_images)
+for i in range(total_images):
+    cv.imwrite("images/Subject_01_" + str(i + 1) + ".png", images[:,:,i])
+
+
+# image = final_mat['manualFluid1']
+# images = image[:,:,16]
+# # print(np.shape(images))
+# row , col = np.shape(images)
+# for i in range(row):
+#     for j in range(col):
+#         if m.isnan(images[i, j]) == True:
+#             images[i, j] = 0
+#         else:
+#             print(images[i,j])
+#
+# cv.imwrite('gray.png', images)
 
 
